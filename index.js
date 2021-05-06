c9ed3b2f21e48158db83e1e888bf9fa5c0113678
bastienwcs <bastien.krettly@wildcodeschool.com>
2021-05-06 10:43:45 +0200

    add post route

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
+const { response } = require('express');
 const express = require('express');
 require('dotenv').config();
 const app = express();
 const connection = require('./config/mysql');
+app.use(express.json());
 
 const port = process.env.PORT;
 
-app.get('/games', function (request, response) {
+app.get('/games', (request, response) => {
   connection
     .promise()
     .query('SELECT * FROM game')
@@ -13,7 +15,7 @@ app.get('/games', function (request, response) {
     .catch((error) => response.status(500).send(error));
 });
 
-app.get('/games/:id', function (request, response) {
+app.get('/games/:id', (request, response) => {
   const { id } = request.params;
   connection
     .promise()
@@ -22,10 +24,28 @@ app.get('/games/:id', function (request, response) {
       if (results.length === 0) {
         response.sendStatus(404);
       } else {
-        response.status(200).json(results)
+        response.status(200).json(results);
       }
     })
     .catch((error) => response.status(500).send(error));
+});
+
+app.post('/games', (request, response) => {
+  const { body } = request;
+
+  connection
+    .promise()
+    .query(`INSERT INTO game (name, rate, publisher_id) VALUES (?, ?, ?)`, [
+      body.name,
+      body.rate,
+      body.publisher_id,
+    ])
+    .then(([results]) => {
+      response.status(201).json(results);
+    })
+    .catch((error) => {
+      response.status(500).json(error);
+    });
 });
 
 app.listen(port, () => {
