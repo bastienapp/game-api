2ee262117b1c66a7f896129f20fccd60a41ac814
bastienwcs <bastien.krettly@wildcodeschool.com>
2021-05-06 10:25:04 +0200

    add get routes

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
 const express = require('express');
 require('dotenv').config();
 const app = express();
+const connection = require('./config/mysql');
 
 const port = process.env.PORT;
 
-app.get('/', function (req, res) {
-  res.send('Hello World');
+app.get('/games', function (request, response) {
+  connection
+    .promise()
+    .query('SELECT * FROM game')
+    .then(([results]) => response.status(200).json(results))
+    .catch((error) => response.status(500).send(error));
+});
+
+app.get('/games/:id', function (request, response) {
+  const { id } = request.params;
+  connection
+    .promise()
+    .query('SELECT * FROM game WHERE id = ?', [id])
+    .then(([results]) => {
+      if (results.length === 0) {
+        response.sendStatus(404);
+      } else {
+        response.status(200).json(results)
+      }
+    })
+    .catch((error) => response.status(500).send(error));
 });
 
 app.listen(port, () => {
