d08aca957280606e2f5f11a05a8e7f4e5366054b
bastienwcs <bastien.krettly@wildcodeschool.com>
2021-05-06 10:02:29 +0200

    add env sample and express logic

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log('start')
+const express = require('express');
+require('dotenv').config();
+const app = express();
+
+const port = process.env.PORT;
+
+app.get('/', function (req, res) {
+  res.send('Hello World');
+});
+
+app.listen(port, () => {
+  console.log(`Connected on port ${port}`);
+});
